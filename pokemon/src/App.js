5fd31209526dc8cc090daac3f97cbd89b11be5e5
Katrinagd <katrinaguyduy@gmail.com>
2019-05-11 17:39:07 -0700

    display pokemon names

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import NavBar from './components/layout/NavBar';
+import Dashboard from './components/layout/Dashboard';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div className="container">
+        <Dashboard></Dashboard>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
