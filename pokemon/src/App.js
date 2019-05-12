06b7da2257340ea0bff43fce029b9c698f17c297
Katrinagd <katrinaguyduy@gmail.com>
2019-05-11 20:05:05 -0700

    Added route

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/layout/NavBar';
 import Dashboard from './components/layout/Dashboard';
+import Pokemon from './components/pokemon/Pokemon';
+
+import backgroundImage from './pattern.png';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <div className="container">
-        <Dashboard></Dashboard>
+      <Router>
+        <div className="App" style={{background: `url(${backgroundImage})`}}>
+          <NavBar />
+          <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard}></Route>
+            <Route exact path="/pokemon/:pokemonIndex" component={Pokemon}></Route>
+          </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
