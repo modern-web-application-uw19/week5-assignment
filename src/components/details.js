efdaac810f7e8fec0c219ac34f98aaf003a8357e
Laura_MS <lmayers3@uw.edu>
2019-05-19 19:56:19 -0400

    basic layout

---
+++ src/components/details.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+export default class Details extends Component {
+    render() {
+        return (
+            <div>details</div>
+        );
+    }
+}
---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+export default class Home extends Component {
+    APICall() {
+       // https://pokeapi.co/api/v2/pokemon/
+    }
+    render() {
+        return (
+            <div>home</div>
+        );
+    }
+}
