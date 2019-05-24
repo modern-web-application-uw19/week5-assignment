9cfbccb2f6086fa583e8e0d5206aa23ed03e973a
Jana Uhrich <janamu@uw.edu>
2019-05-23 23:28:34 -0700

    homework attempt

---
+++ catchemall/src/PokemonDetail.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+
+let pokeNumber = 1;
+
+const pokeTopTen = function () {
+  for (let i = 0; i < 10; i++) {
+    pokeNumber++;
+    return pokeNameGetter(this.state.data);
+  }
+ } 
+
+const pokeNameGetter = function(poke) { 
+  return poke.forms[0].name;
+}
+
+
+class Pokemon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      isLoading: true,
+      hasError: false
+    }
+  }
+  componentDidMount() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNumber}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          data: data,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        this.setState({
+          hasError: true,
+          isLoading: false
+        });
+      });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    if (this.state.hasError) {
+      return <div>ERROR, please reload and try again</div>;
+    }
+
+    //const names = Object.keys(this.state.data)
+     // .map((names, idx) => <li key={idx}>{names}</li>);
+
+
+    return (
+      <div>
+        <ul>
+          {pokeTopTen}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function Home() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+function NotFound() {
+  return <h1>404</h1>
+}
+
+function TheRouter() {
+  return (
+    <div>
+      <Router>
+        <Link to="/">Home </Link>
+        <Link to="/pokemon/:name">Pokemon</Link>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/pokemon/:name" component={Pokemon} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default TheRouter;
